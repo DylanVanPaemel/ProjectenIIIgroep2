f936408cd45543e38fba9bd0628369c2fdb68df4
ProjectenIIIgroep2 <dylanvanpaemel@gmail.com>
2017-11-09 11:13:40 +0100

    page access

---
+++ sites/default/files/php/twig/59e504a0bbf22_inline-template_l7eeVeH82BUEsIatnvD7Hqlla/AOiRKeQWi59J4uhCiIcMMWfZ6mjFib1nb5SgBn_bzRQ.php
@@ -0,0 +1,60 @@
+<?php
+
+/* {# inline_template_start #}user/10 */
+class __TwigTemplate_70e9304f737251e9482380de0e1c606c1afd5d8999fa2fb816bc6457c7e94566 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "user/10";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}user/10";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{# inline_template_start #}user/10";
+    }
+}
